a3426eddcf9f3cc961206f315c88fe0cf19257a5
Zerk <axzerk@gmail.com>
2016-11-25 13:41:25 +0200

    finished js nav switch

--- app/js/common.js
+++ app/js/common.js
@@ -1,69 +1,78 @@
 // Initialising primary slider controls clickability
-
 var primarySlider = {
-  el: {
-    primarySlider: $(".slider_role_main"),
-    sliderNav: $(".slider_role_main .slider__nav"),
-    allNavButtons: $(".slider_role_main .slider__control")
-  },
+    el: {
+        primarySlider: $(".slider_role_main"),
+        sliderNav: $(".slider_role_main .slider__nav"),
+        allNavButtons: $(".slider_role_main .slider__control")
+    },
 
-  init: function() {
-    this.bindUIEvents();
-  },
+    init: function() {
+        this.bindUIEvents();
+    },
 
-  bindUIEvents: function() {
-    this.el.sliderNav.on("click", ".slider__control", function(event) {
-      primarySlider.handleNavClick(event, this);
-    });
-  },
+    bindUIEvents: function() {
+        this.el.sliderNav.on("click", ".slider__control", function(event) {
+            primarySlider.handleNavClick(event, this);
+        });
+    },
 
-  handleNavClick: function(event, el) {
-    event.preventDefault();
-    this.changeActiveNav(el);
-  },
+    handleNavClick: function(event, el) {
+        event.preventDefault();
+        this.changeActiveNav(el);
+    },
 
-  changeActiveNav: function(el) {
-    this.el.allNavButtons.removeClass("slider__control_active");
-    $(el).addClass("slider__control_active");
-  }
-};
-
-// Initialising secondary slider controls clickability
-var testimonialsSlider = {
-  el: {
-    testimonialsSlider: $(".b-content__slider"),
-    sliderNav: $(".b-content__slider .slider__nav"),
-    allNavButtons: $(".b-content__slider .slider__control")
-  },
-
-  init: function() {
-    this.bindUIEvents();
-  },
-
-  bindUIEvents: function() {
-    this.el.sliderNav.on("click", ".slider__control", function(event) {
-      testimonialsSlider.handleNavClick(event, this);
-    });
-  },
-
-  handleNavClick: function(event, el) {
-    event.preventDefault();
-    this.changeActiveNav(el);
-  },
-
-  changeActiveNav: function(el) {
-    this.el.allNavButtons.removeClass("slider__control_active");
-    $(el).addClass("slider__control_active");
-  }
+    changeActiveNav: function(el) {
+        this.el.allNavButtons.removeClass("slider__control_active");
+        $(el).addClass("slider__control_active");
+    }
 };
 
 primarySlider.init();
+
+// Initialising secondary slider controls clickability
+var testimonialsSlider = {
+    el: {
+        testimonialsSlider: $(".b-content__slider"),
+        sliderNav: $(".b-content__slider .slider__nav"),
+        allNavButtons: $(".b-content__slider .slider__control")
+    },
+
+    init: function() {
+        this.bindUIEvents();
+    },
+
+    bindUIEvents: function() {
+        this.el.sliderNav.on("click", ".slider__control", function(event) {
+            testimonialsSlider.handleNavClick(event, this);
+        });
+    },
+
+    handleNavClick: function(event, el) {
+        event.preventDefault();
+        this.changeActiveNav(el);
+    },
+
+    changeActiveNav: function(el) {
+        this.el.allNavButtons.removeClass("slider__control_active");
+        $(el).addClass("slider__control_active");
+    }
+};
+
 testimonialsSlider.init();
 
+// Adding\removing nav class modifier on window resize
+function jqUpdateSize() {
+    var wSize = $(window).width();
+        if ($(this).width() >= 768) {
+            $('#main-nav')
+                .removeClass('nav_view_vertical')
+                .addClass('nav_view_horizontal');
+        } else {
+            $('#main-nav')
+                .removeClass('nav_view_horizontal')
+                .addClass('nav_view_vertical');
+        }
+};
 
-// if ($(window).width() < 960) {
-//    alert('Less than 960');
-// }
-// else {
-//    alert('More than 960');
-// }
+$(document).ready(jqUpdateSize);
+$(window).resize(jqUpdateSize);
