0275efc28d981aaea3e2ffed480b269e0986749a
Zerk <axzerk@gmail.com>
2016-11-25 11:31:13 +0200

    finished media queries

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,66 @@
+// Initialising primary slider controls clickability
+
+var primarySlider = {
+  el: {
+    primarySlider: $(".slider_role_main"),
+    sliderNav: $(".slider_role_main .slider__nav"),
+    allNavButtons: $(".slider_role_main .slider__control")
+  },
+
+  init: function() {
+    this.bindUIEvents();
+  },
+
+  bindUIEvents: function() {
+    this.el.sliderNav.on("click", ".slider__control", function(event) {
+      primarySlider.handleNavClick(event, this);
+    });
+  },
+
+  handleNavClick: function(event, el) {
+    event.preventDefault();
+    this.changeActiveNav(el);
+  },
+
+  changeActiveNav: function(el) {
+    this.el.allNavButtons.removeClass("slider__control_active");
+    $(el).addClass("slider__control_active");
+  }
+};
+
+// Initialising secondary slider controls clickability
+var testimonialsSlider = {
+  el: {
+    testimonialsSlider: $(".b-content__slider"),
+    sliderNav: $(".b-content__slider .slider__nav"),
+    allNavButtons: $(".b-content__slider .slider__control")
+  },
+
+  init: function() {
+    this.bindUIEvents();
+  },
+
+  bindUIEvents: function() {
+    this.el.sliderNav.on("click", ".slider__control", function(event) {
+      testimonialsSlider.handleNavClick(event, this);
+    });
+  },
+
+  handleNavClick: function(event, el) {
+    event.preventDefault();
+    this.changeActiveNav(el);
+  },
+
+  changeActiveNav: function(el) {
+    this.el.allNavButtons.removeClass("slider__control_active");
+    $(el).addClass("slider__control_active");
+  }
+};
+
+primarySlider.init();
+testimonialsSlider.init();
+
+
 // if ($(window).width() < 960) {
 //    alert('Less than 960');
 // }
