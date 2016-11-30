8ac7554b24866b3b5c21c474a371c5cdfa22d5b3
Zerk <axzerk@gmail.com>
2016-11-30 11:47:44 +0200

    kinda ie8 viable, not rly

--- app/js/common.js
+++ app/js/common.js
@@ -1,64 +1,64 @@
 // Initialising primary slider controls clickability
-var primarySlider = {
-    el: {
-        primarySlider: $(".slider_role_main"),
-        sliderNav: $(".slider_role_main .slider__nav"),
-        allNavButtons: $(".slider_role_main .slider__control")
-    },
-
-    init: function() {
-        this.bindUIEvents();
-    },
-
-    bindUIEvents: function() {
-        this.el.sliderNav.on("click", ".slider__control", function(event) {
-            primarySlider.handleNavClick(event, this);
-        });
-    },
-
-    handleNavClick: function(event, el) {
-        event.preventDefault();
-        this.changeActiveNav(el);
-    },
-
-    changeActiveNav: function(el) {
-        this.el.allNavButtons.removeClass("slider__control_active");
-        $(el).addClass("slider__control_active");
-    }
-};
-
-primarySlider.init();
+// var primarySlider = {
+//     el: {
+//         primarySlider: $(".slider_role_main"),
+//         sliderNav: $(".slider_role_main .slider__nav"),
+//         allNavButtons: $(".slider_role_main .slider__control")
+//     },
+//
+//     init: function() {
+//         this.bindUIEvents();
+//     },
+//
+//     bindUIEvents: function() {
+//         this.el.sliderNav.on("click", ".slider__control", function(event) {
+//             primarySlider.handleNavClick(event, this);
+//         });
+//     },
+//
+//     handleNavClick: function(event, el) {
+//         event.preventDefault();
+//         this.changeActiveNav(el);
+//     },
+//
+//     changeActiveNav: function(el) {
+//         this.el.allNavButtons.removeClass("slider__control_active");
+//         $(el).addClass("slider__control_active");
+//     }
+// };
+//
+// primarySlider.init();
 
 // Initialising secondary slider controls clickability
-var testimonialsSlider = {
-    el: {
-        testimonialsSlider: $(".b-content__slider"),
-        sliderNav: $(".b-content__slider .slider__nav"),
-        allNavButtons: $(".b-content__slider .slider__control")
-    },
-
-    init: function() {
-        this.bindUIEvents();
-    },
-
-    bindUIEvents: function() {
-        this.el.sliderNav.on("click", ".slider__control", function(event) {
-            testimonialsSlider.handleNavClick(event, this);
-        });
-    },
-
-    handleNavClick: function(event, el) {
-        event.preventDefault();
-        this.changeActiveNav(el);
-    },
-
-    changeActiveNav: function(el) {
-        this.el.allNavButtons.removeClass("slider__control_active");
-        $(el).addClass("slider__control_active");
-    }
-};
-
-testimonialsSlider.init();
+// var testimonialsSlider = {
+//     el: {
+//         testimonialsSlider: $(".b-content__slider"),
+//         sliderNav: $(".b-content__slider .slider__nav"),
+//         allNavButtons: $(".b-content__slider .slider__control")
+//     },
+//
+//     init: function() {
+//         this.bindUIEvents();
+//     },
+//
+//     bindUIEvents: function() {
+//         this.el.sliderNav.on("click", ".slider__control", function(event) {
+//             testimonialsSlider.handleNavClick(event, this);
+//         });
+//     },
+//
+//     handleNavClick: function(event, el) {
+//         event.preventDefault();
+//         this.changeActiveNav(el);
+//     },
+//
+//     changeActiveNav: function(el) {
+//         this.el.allNavButtons.removeClass("slider__control_active");
+//         $(el).addClass("slider__control_active");
+//     }
+// };
+//
+// testimonialsSlider.init();
 
 // Adding/removing nav class modifier on window resize
 // Adding/removing slider-img src on breakpoints
