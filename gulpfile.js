b1cc3724dcae98dee11572eaa863cf2d0fb2f1d9
Zerk <axzerk@gmail.com>
2016-12-05 19:44:45 +0200

    mostly done

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,15 @@
-var gulp 					= require('gulp'),
-		sass 				= require('gulp-sass'),
-		concat 				= require('gulp-concat'),
-		uncss 				= require('gulp-uncss');
-		browserSync			= require('browser-sync'),
-		del 				= require('del'),
-		imagemin			= require('gulp-imagemin'),
-		pngquant 			= require('imagemin-pngquant'),
-		cache 				= require('gulp-cache'),
-		autoprefixer 		= require('gulp-autoprefixer'),
-		postcss    			= require('gulp-postcss'),
-		plugins 			= require('gulp-load-plugins')();
+var gulp 				= require('gulp'),
+	sass 				= require('gulp-sass'),
+	concat 				= require('gulp-concat'),
+	cssnano             = require('gulp-cssnano'),
+	cssBeautify 		= require('gulp-cssbeautify'),
+	browserSync			= require('browser-sync'),
+	del 				= require('del'),
+	imagemin			= require('gulp-imagemin'),
+	pngquant 			= require('imagemin-pngquant'),
+	cache 				= require('gulp-cache'),
+	autoprefixer 		= require('gulp-autoprefixer'),
+	plugins 			= require('gulp-load-plugins')();
 
 var paths = {
   	srcSass    : ['app/blocks/**/*.sass'],
@@ -19,29 +19,35 @@ var paths = {
   	destJs     : 'app/js'
 };
 
+// Assembling .sass files
 gulp.task('bundleCss' , function(){
 	gulp.src(paths.srcSass)
 	.pipe(plugins.sass({outputStyle: 'expanded'}).on('error', plugins.sass.logError))
 	.pipe(plugins.concat('style.css'))
-	// .pipe(plugins.uncss({html: [paths.srcHtml]}))
-	// .pipe(postcss([require('postcss-flexibility')]))
-	.pipe(autoprefixer(['last 15 versions', '> 2%', 'ie 8']))
+	.pipe(autoprefixer(['last 15 versions', '> 2%', 'ie 8'], { cascade: true }))
+	// .pipe(plugins.cssnano())
+	// .pipe(cssBeautify())
 	.pipe(gulp.dest(paths.destCss))
 	.pipe(browserSync.reload({
 		stream: true
 	}))
 });
 
+// Assembling .js files
 gulp.task('bundleJs', function() {
 	gulp.src(paths.srcJs)
 		.pipe(plugins.concat('common.js'))
 		.pipe(gulp.dest(paths.destJs))
+		.pipe(browserSync.reload({
+			stream: true
+		}))
 });
 
+
 gulp.task('watch', function(){
-	gulp.watch(paths.srcSass, ['bundleCss'])
 	gulp.watch(paths.srcHtml, browserSync.reload);
-	gulp.watch(paths.srcJs, browserSync.reload);
+	gulp.watch(paths.srcSass, {cwd: './'}, ['bundleCss'])
+	gulp.watch(paths.srcJs, {cwd: './'}, ['bundleJs']);
 });
 
 gulp.task('browser-sync', function(){
@@ -53,10 +59,12 @@ gulp.task('browser-sync', function(){
 	});
 });
 
+// Cleaning Dist folder
 gulp.task('clean-dist', function() {
-	del.sync('dist');
+	return del.sync('dist');
 });
 
+// Clearning cache
 gulp.task('clean-cache', function() {
 	cache.clearAll();
 });
@@ -72,6 +80,7 @@ gulp.task('bundleImg', function() {
 	.pipe(gulp.dest('dist/img'));
 });
 
+// Building our app
 gulp.task ('build', ['clean-dist', 'bundleImg', 'bundleCss'], function() {
 	var buildCss = gulp.src('app/css/style.css')
 		.pipe(gulp.dest('dist/css'));
--- readme.txt
+++
@@ -1,7 +0,0 @@
-GoIT-FE homework #9
-
-Student: Alexander Repeta
-
-Ссылка на готовую страницу: https://axzerk.github.io/app
-
-Сделать макет с 7-8 домашки кроссбраузерным.
