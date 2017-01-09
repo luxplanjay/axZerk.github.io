b0662beed6e5c8cd121f71d97bb38124f8ff49da
Zerk <axzerk@gmail.com>
2017-01-09 13:38:18 +0200

    js 7-8 initial commit

--- src/blocks/login-form/login-form.js
+++ src/blocks/login-form/login-form.js
@@ -1,7 +1,3 @@
-(function () {
-  'use strict';
-})();
-
 $(function() {
     let $inputs = $('.login-form__input'),
         showBtn = $('.login-form__btn');
@@ -50,48 +46,4 @@ $(function() {
 
         return tooltipsList;
     }
-});
-
-$(function () {
-  let $links = $('.nav__link'),
-      $tabs = $('.tabs__item');
-
-  // Initializing default gadget state
-  initGadget($links, $tabs);
-
-  // On hover event
-  $links.hover(function (e) {
-    $(this).addClass('nav__link_hover');
-  }, function (e) {
-    $(this).removeClass('nav__link_hover');
-  });
-
-  // On click event
-  $links.click(function (e) {
-    e.preventDefault();
-
-    let currentLink = $(this),
-        tabId = currentLink.attr('href');
-
-    toggleActiveLink($links, currentLink);
-    toggleTabs($tabs, tabId);
-  });
-
-  // Setting default gadget state
-  function initGadget(links, tabs) {
-    toggleActiveLink(links, links.first());
-    toggleTabs(tabs, tabs.first());
-  }
-
-  // Active link toggle
-  function toggleActiveLink(links, current) {
-    links.removeClass('nav__link_active');
-    current.addClass('nav__link_active');
-  }
-
-  // Active tab toggle
-  function toggleTabs(tabs, tabId) {
-    tabs.hide();
-    $(tabId).fadeIn(300);
-  }
 });
