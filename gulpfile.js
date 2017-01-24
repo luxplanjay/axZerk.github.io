c073dff490ee83d8d02837b71643b4f29369cf49
Zerk <axzerk@gmail.com>
2017-01-24 20:29:18 +0200

    reset

--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,107 @@
-var gulp 								= require('gulp'),
-		sass 								= require('gulp-sass'),
-		concat 							= require('gulp-concat'),
-		cssnano             = require('gulp-cssnano'),
-		browserSync					= require('browser-sync'),
-		del 								= require('del'),
-		imagemin						= require('gulp-imagemin'),
-		pngquant 						= require('imagemin-pngquant'),
-		cache 							= require('gulp-cache'),
-		autoprefixer 				= require('gulp-autoprefixer'),
-		plugins 						= require('gulp-load-plugins')();
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    cssnano = require('gulp-cssnano'),
+    browserSync = require('browser-sync'),
+    del = require('del'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
+    cache = require('gulp-cache'),
+    autoprefixer = require('gulp-autoprefixer'),
+    plugins = require('gulp-load-plugins')(),
+    rigger = require('gulp-rigger'),
+    prettify = require('gulp-html-prettify');
 
 var paths = {
-		srcSass: ['./src/blocks/core/*.scss', './src/blocks/**/*.scss'],
-  	srcJs: './src/blocks/**/*.js',
-  	srcHtml: './src/index.html',
-		srcSvg: './src/**/*.svg',
-  	destCss: './src/css',
-  	destJs: './src/js',
-		destSvg: './src/img'
+    globalImport: './src/blocks/core/tools/',
+
+    srcSass: ['./src/blocks/core/*.scss', './src/blocks/**/*.scss'],
+    srcJs: './src/blocks/**/*.js',
+
+    srcHtml: './src/html/index.html',
+
+    srcSvg: './src/**/*.svg',
+    destCss: './src/css',
+    destJs: './src/js',
+    destSvg: './src/img',
+
+    destHtml: './src/index.html'
 };
 
 // Assembling .scss files
-gulp.task('bundleCss' , function(){
-	gulp.src(paths.srcSass)
-		.pipe(plugins.concat('style.css'))
-		.pipe(plugins.sass({
-				outputStyle: 'expanded',
-				includePaths: ['node_modules/susy/sass']
-		}).on('error', plugins.sass.logError))
-		.pipe(autoprefixer({browsers: ['last 5 versions', 'IE 8', 'IE 9'], cascade: true }))
-		.pipe(plugins.cssnano())
-		.pipe(gulp.dest(paths.destCss))
-		.pipe(browserSync.reload({ stream: true }));
+gulp.task('bundleCss', function() {
+  gulp.src(paths.srcSass)
+    .pipe(plugins.sassGlobImport())
+    .pipe(plugins.sass({
+        outputStyle: 'expanded',
+        includePaths: [paths.globalImport, 'node_modules/susy/sass']
+    }).on('error', plugins.sass.logError))
+    .pipe(plugins.concat('style.css'))
+    .pipe(autoprefixer({
+        browsers: ['last 5 versions', 'IE 8', 'IE 9'],
+        cascade: true
+    }))
+    // .pipe(plugins.cssnano())
+    .pipe(gulp.dest(paths.destCss))
+    .pipe(browserSync.reload({
+        stream: true
+    }));
 });
+
+// Assembling index html
+gulp.task('rigger', function() {
+    gulp.src(paths.srcHtml)
+        .pipe(rigger())
+        .pipe(prettify({
+            indent_char: '  ',
+            indent_size: 1
+        }))
+        .pipe(gulp.dest('src/'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+// gulp.task('prettify', function() {
+//     gulp.src(paths.destHtml)
+//         .pipe(prettify({
+//             indent_char: '  ',
+//             indent_size: 1
+//         }))
+//         .pipe(gulp.dest(paths.destHtml));
+// });
+
 
 // Assembling .js files
 gulp.task('bundleJs', function() {
-	gulp.src(paths.srcJs)
-		.pipe(plugins.concat('common.js'))
-		.pipe(gulp.dest(paths.destJs))
-		.pipe(browserSync.reload({ stream: true }));
+    gulp.src(paths.srcJs)
+        .pipe(plugins.concat('common.js'))
+        .pipe(gulp.dest(paths.destJs))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
 
-gulp.task('watch', function(){
-	gulp.watch(paths.srcHtml, browserSync.reload);
-	gulp.watch(paths.srcSass, {cwd: './'}, ['bundleCss']);
-	gulp.watch(paths.srcJs, {cwd: './'}, ['bundleJs']);
+gulp.task('watch', function() {
+    gulp.watch(paths.destHtml, browserSync.reload);
+    gulp.watch(paths.srcSass, {
+        cwd: './'
+    }, ['bundleCss']);
+    gulp.watch(paths.srcJs, {
+        cwd: './'
+    }, ['bundleJs']);
+    gulp.watch('src/html/*.html', ['rigger']);
+
 });
 
-gulp.task('browser-sync', function(){
-	browserSync({
-		server: {
-			baseDir: 'src'
-		},
-		notify: false
-	});
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: 'src'
+        },
+        notify: false
+    });
 });
 
 // Cleaning Dist folder
@@ -94,4 +140,4 @@ gulp.task('browser-sync', function(){
 // 		.pipe(gulp.dest('dist'));
 // });
 
-gulp.task('default', ['browser-sync', 'bundleCss', 'bundleJs', 'watch']);
+gulp.task('default', ['browser-sync', 'rigger', 'bundleCss', 'bundleJs', 'watch']);
--- src/blocks/selectric/selectric.js
+++
@@ -1,3 +0,0 @@
-$(function() {
-  $('.custom-select').selectric();
-});
