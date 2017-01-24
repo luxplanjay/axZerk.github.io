32d511fccf275ae234993739a8d4210c44e9b362
Zerk <axzerk@gmail.com>
2017-01-24 16:07:27 +0200

    added stop() method to animations to prevent queues

--- src/js/common.js
+++ src/js/common.js
@@ -138,31 +138,32 @@ $(function () {
       $sublinks = $('.sub-menu__link');
 
   $links.hover(function () {  // in
-    $(this).animate({
+    $(this).stop().animate({
       backgroundColor: 'maroon'
     }, 300);
   }, function () {  // out
-    $(this).animate({
+    $(this).stop().animate({
       backgroundColor: '#333'
     }, 300);
   });
 
   $dropdown.hover(function () { // in
-    $(this).children('.sub-menu').animate({
+    $(this).children('.sub-menu').stop().animate({
       height: 'toggle'
     }, 300);
   }, function () {  // out
-    $(this).children('.sub-menu').animate({
+    $(this).children('.sub-menu').stop().animate({
       height: 'toggle'
     }, 300);
   });
+
   $sublinks.hover(function () { // in
-    $(this).animate({
+    $(this).stop().animate({
       backgroundColor: '#cc8400',
       color: '#000',
     }, 300);
   }, function () {  // out
-    $(this).animate({
+    $(this).stop().animate({
       backgroundColor: '#333',
       color: '#fefefe'
     }, 300);
