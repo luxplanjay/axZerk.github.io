57e2a417a316788a76ca73b3a1852b4905b76c82
Zerk <axzerk@gmail.com>
2017-01-27 03:50:13 +0200

    finished js 11-12

---
+++ src/blocks/page/page.js
@@ -0,0 +1,11 @@
+$(function () {
+
+  // Initializing zCarousel
+  $('.zCarousel').zCarousel({
+    backgroundColor: '#333',
+    color: '#e65722',
+    hoverColor: '#fefefe',
+    fontSize: '20px'
+  });
+  
+});
