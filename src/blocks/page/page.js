2d959a062a6d579bb0d3932a944f04bca3f057f8
Zerk <axzerk@gmail.com>
2017-01-28 23:49:45 +0200

    Hard reset

--- src/blocks/page/page.js
+++ src/blocks/page/page.js
@@ -1,11 +1,1 @@
-$(function () {
 
-  // Initializing zCarousel
-  $('.zCarousel').zCarousel({
-    backgroundColor: '#333',
-    color: '#e65722',
-    hoverColor: '#fefefe',
-    fontSize: '20px'
-  });
-  
-});
