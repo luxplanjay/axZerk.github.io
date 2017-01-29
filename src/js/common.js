0bb7e2e24687d63487e5108d8485a226dea64b03
Zerk <axzerk@gmail.com>
2017-01-29 14:17:54 +0200

    Async file load test

---
+++ src/js/common.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', function () {
+    "use strict";
+
+    const articleList = [
+        {
+            title: 'Article Title 1',
+            text: 'Lorem ipsum',
+        },
+        {
+            title: 'Article Title 2',
+            text: 'Lorem ipsum',
+        },
+        {
+            title: 'Article Title 3',
+            text: 'Lorem ipsum',
+        }
+    ];
+
+    function loadTemplate (path) {
+        return new Promise(function(resolve, reject) {
+            let request = new XMLHttpRequest();
+            request.onload = function () {
+                resolve(this.responseText);
+            };
+
+            request.onerror = reject;
+            request.open('GET', path, true);
+            request.send();
+        });
+    }
+
+    loadTemplate('blocks/templates/test.hbs').then(function (result) {
+        let template = Handlebars.compile(result);
+        let html = template(articleList);
+    }).catch(function () {
+        alert('ERROR!');
+    });
+});
+
+// let test = JSON.stringify(articleList);
+// // console.log(test);
+// localStorage.setItem('users', test);
+//
+// let getFromStorage = localStorage.getItem('users');
+// console.log('From local storage: ', getFromStorage);
+// console.log('type is: ', typeof getFromStorage);
+//
+// let obj = JSON.parse(getFromStorage);
+// console.log(obj);
