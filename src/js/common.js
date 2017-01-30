e98dcf1f74b13969708b084a744bf96d9d2b1acd
Zerk <axzerk@gmail.com>
2017-01-30 19:25:21 +0200

    finished loadJSON foo

--- src/js/common.js
+++ src/js/common.js
@@ -1,24 +1,31 @@
 document.addEventListener('DOMContentLoaded', function () {
     "use strict";
 
-    const articleList = [
-        {
-            title: 'Article Title 1',
-            text: 'Lorem ipsum',
-        },
-        {
-            title: 'Article Title 2',
-            text: 'Lorem ipsum',
-        },
-        {
-            title: 'Article Title 3',
-            text: 'Lorem ipsum',
-        }
-    ];
+    // USING PROMISE API
+    generateForm();
 
-    function loadTemplate (path) {
-        return new Promise(function(resolve, reject) {
+    function generateForm() {
+        loadJSON('blocks/json/test.json')
+            .then(function (result) {
+
+                let obj = JSON.parse(result);
+                //Creating Handlebars template function
+                let template = Handlebars.templates.test;
+
+                //Creating actual HTML string using Handlebars template function
+                let resultHtml = template(obj);
+
+                //Appending ressulting HTML string to body just for test
+                let body = document.querySelector('body');
+                body.innerHTML = resultHtml;
+            });
+    }
+
+    function loadJSON(path) {
+        return new Promise(function (resolve, reject) {
             let request = new XMLHttpRequest();
+            request.overrideMimeType("application/json");
+
             request.onload = function () {
                 resolve(this.responseText);
             };
@@ -28,22 +35,4 @@ document.addEventListener('DOMContentLoaded', function () {
             request.send();
         });
     }
-
-    loadTemplate('blocks/templates/test.hbs').then(function (result) {
-        let template = Handlebars.compile(result);
-        let html = template(articleList);
-    }).catch(function () {
-        alert('ERROR!');
-    });
 });
-
-// let test = JSON.stringify(articleList);
-// // console.log(test);
-// localStorage.setItem('users', test);
-//
-// let getFromStorage = localStorage.getItem('users');
-// console.log('From local storage: ', getFromStorage);
-// console.log('type is: ', typeof getFromStorage);
-//
-// let obj = JSON.parse(getFromStorage);
-// console.log(obj);
