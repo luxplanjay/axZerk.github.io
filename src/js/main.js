f6500dabff62f34614ff81ec14e0f9f970685bde
Zerk <axzerk@gmail.com>
2017-02-01 00:13:06 +0200

    resolving requireJS

--- src/js/main.js
+++ src/js/main.js
@@ -6,10 +6,13 @@ requirejs(['model'], function (model) {
 
 
 requirejs(['view'], function (view) {
+    requirejs('model');
 });
 
 
 requirejs(['controller'], function (controller) {
+    requirejs('model');
+    requirejs('view');
 });
 
 
