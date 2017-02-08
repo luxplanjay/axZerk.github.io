f45b9fc83d5446818edd877d3afb96dd73183a7a
Zerk <axzerk@gmail.com>
2017-02-09 01:53:57 +0200

    mostly done

---
+++ src/blocks/main-content/accordion/accordion.js
@@ -0,0 +1,43 @@
+/**
+ * Created by Zerk on 08-Feb-17.
+ */
+
+$(function () {
+    (function () {
+        let $links = $('.accordion__link'),
+            $icons = $('.accordion__icon'),
+            $content = $('.accordion__content');
+
+        $links.hover(function () {
+            $(this).find('.accordion__icon').addClass('accordion__icon_hover');
+        }, function () {
+            $(this).find('.accordion__icon').removeClass('accordion__icon_hover');
+        });
+
+        $links.click(function (e) {
+            e.preventDefault();
+
+            if ($(this).hasClass('accordion__link_active')) {
+
+                $(this).removeClass("accordion__link_active");
+                $(this).siblings('.accordion__content').slideUp(300);
+                $icons.removeClass('accordion__icon_active');
+                $icons.removeClass("fa-minus").addClass("fa-plus");
+
+            } else {
+                $icons.removeClass('accordion__icon_active');
+                $(this).find('.accordion__icon').addClass('accordion__icon_active');
+
+                $icons.removeClass("fa-minus").addClass("fa-plus");
+                $(this).find('.accordion__icon').removeClass("fa-plus").addClass("fa-minus");
+
+                $links.removeClass("accordion__link_active");
+                $(this).addClass("accordion__link_active");
+
+                $content.slideUp(300);
+                $(this).siblings('.accordion__content').slideDown(300);
+            }
+        });
+    })();
+
+});
---
+++ src/js/main.js
@@ -0,0 +1,71 @@
+$(function () {
+    // init
+    let $slider = $('.slider');
+
+    $slider.jcarousel({});
+
+    // Slider pagination
+    let pagination = $('.slider-pagination');
+
+    pagination.jcarouselPagination({
+        item: function (page) {
+            return '<a href="#' + page + '">' + page + '</a>';
+        }
+    });
+
+    // making first link active on page load
+    $('.slider-pagination a:first-child').addClass('slider-pagination__link_active');
+
+    pagination
+        .on('jcarouselpagination:active', 'a', function () {
+            $(this).addClass('slider-pagination__link_active');
+        })
+        .on('jcarouselpagination:inactive', 'a', function () {
+            $(this).removeClass('slider-pagination__link_active');
+        });
+
+});
+
+/**
+ * Created by Zerk on 08-Feb-17.
+ */
+
+$(function () {
+    (function () {
+        let $links = $('.accordion__link'),
+            $icons = $('.accordion__icon'),
+            $content = $('.accordion__content');
+
+        $links.hover(function () {
+            $(this).find('.accordion__icon').addClass('accordion__icon_hover');
+        }, function () {
+            $(this).find('.accordion__icon').removeClass('accordion__icon_hover');
+        });
+
+        $links.click(function (e) {
+            e.preventDefault();
+
+            if ($(this).hasClass('accordion__link_active')) {
+
+                $(this).removeClass("accordion__link_active");
+                $(this).siblings('.accordion__content').slideUp(300);
+                $icons.removeClass('accordion__icon_active');
+                $icons.removeClass("fa-minus").addClass("fa-plus");
+
+            } else {
+                $icons.removeClass('accordion__icon_active');
+                $(this).find('.accordion__icon').addClass('accordion__icon_active');
+
+                $icons.removeClass("fa-minus").addClass("fa-plus");
+                $(this).find('.accordion__icon').removeClass("fa-plus").addClass("fa-minus");
+
+                $links.removeClass("accordion__link_active");
+                $(this).addClass("accordion__link_active");
+
+                $content.slideUp(300);
+                $(this).siblings('.accordion__content').slideDown(300);
+            }
+        });
+    })();
+
+});
